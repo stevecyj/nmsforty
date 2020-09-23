02756b8b4335dfca3039d77b3f063dd79414816a
Steve <ycsteven.tsao@gmail.com>
2020-09-23 15:13:26 +0800

    use grid, remove ANSI

--- src/views/nms/test.vue
+++ src/views/nms/test.vue
@@ -12,7 +12,8 @@
       </a-breadcrumb-item>
       <a-breadcrumb-separator />
       <a-breadcrumb-item>An Application</a-breadcrumb-item>
-    </a-breadcrumb> -->
+    </a-breadcrumb>-->
+
     <a-page-header
       style="border: 1px solid rgb(235, 237, 240)"
       title="Title"
@@ -20,19 +21,23 @@
       @back="() => null"
     />
 
-    <a-select
-      label-in-value
-      :default-value="{ key: 'lucy' }"
-      style="width: 120px"
-      @change="handleChange"
-    >
-      <a-select-option value="jack">
-        Jack (100)
-      </a-select-option>
-      <a-select-option value="lucy">
-        Lucy (101)
-      </a-select-option>
-    </a-select>
+    <a-row :gutter="[8,8]">
+      <a-col :span="4">
+        <a-select
+          label-in-value
+          :default-value="{ key: 'lucy' }"
+          style="width: 300px"
+          size="large"
+          @change="handleChange"
+        >
+          <a-select-option value="jack">Jack (100)</a-select-option>
+          <a-select-option value="lucy">Lucy (101)</a-select-option>
+        </a-select>
+      </a-col>
+      <a-col :span="20">
+        <a-input-search placeholder="input search text" enter-button="Search" size="large" @search="onSearch" />
+      </a-col>
+    </a-row>
   </div>
 </template>
 
@@ -43,18 +48,21 @@
 
 <script>
 export default {
-  methods: {
-    handleChange(value) {
-      console.log(value) // { key: "lucy", label: "Lucy (101)" }
+    methods: {
+        handleChange(value) {
+            console.log(value) // { key: "lucy", label: "Lucy (101)" }
+        },
+        onSearch(value) {
+            console.log(value)
+        }
     }
-  }
 }
 </script>
 
 <style lang="less" scoped>
 @import '~ant-design-vue/lib/style/themes/default.less';
 h2 {
-  color: goldenrod;
-  font-size: 26px;
+    color: goldenrod;
+    font-size: 26px;
 }
 </style>
